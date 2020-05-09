34b90043569beaa06437f2faed15657f4aa950d7
Joshua Wood <josh@joshuawood.net>
2020-05-08 17:46:05 -0700

    badgers!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   def index
     render html: "<h1>Hello Lamby</h1>".html_safe
   end
+
+  def badgers
+    fail 'badgers!'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root to: "application#index"
+  get "/badgers", to: "application#badgers"
 end
